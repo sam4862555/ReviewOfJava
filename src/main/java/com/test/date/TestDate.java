b173a160dc8fed7e3c1e2a17b3c3603ecd08546e
sunzhenhua <870991173@qq.com>
2017-08-07 21:31:39 +0800

    add component

---
+++ src/main/java/com/test/date/TestDate.java
@@ -0,0 +1,37 @@
+package com.test.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Created by szh on 2017/8/7.
+ */
+public class TestDate {
+
+    public static void main(String[] args){
+
+        //Original Version
+        String date = "2016-07-03";
+        SimpleDateFormat converter = new SimpleDateFormat("yyyy-MM-dd"); //Locale.CHINA
+        SimpleDateFormat converter2 = new SimpleDateFormat("yyyy MM dd HH:mm:ss");
+        try {
+            Date ks = converter.parse(date);
+            System.out.println(ks.getMonth());
+            String time = converter2.format(ks);
+            System.out.println(time);
+
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+
+        //JodaTime Version
+        
+
+
+
+    }
+
+}
